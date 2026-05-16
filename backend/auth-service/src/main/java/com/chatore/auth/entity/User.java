4e30d90e3c6126cd3c9b694a5c7fe0535e0143ab
Syed Ayat Ahmad Rizvi <sayeadahmad@gmail.com>
2026-05-16 23:59:06 +0530

    code(setup): initialize backend multi-module architecture

---
+++ backend/auth-service/src/main/java/com/chatore/auth/entity/User.java
@@ -0,0 +1,72 @@
+package com.chatore.auth.entity;
+
+
+import com.chatore.auth.entity.enums.AccountStatus;
+import com.chatore.auth.entity.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private Boolean isEmailVerified;
+
+    @Column(nullable = false)
+    private Boolean isPhoneVerified;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AccountStatus accountStatus;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @Column(nullable = false)
+    private LocalDateTime lastLoginAt;
+
+//    id
+//            fullName
+//    email
+//            phoneNumber
+//    password
+//            role
+//    isEmailVerified
+//            isPhoneVerified
+//    accountStatus
+//            lastLoginAt
+
+}
---
+++ backend/auth-service/src/main/java/com/chatore/auth/entity/enums/UserRole.java
@@ -0,0 +1,8 @@
+package com.chatore.auth.entity.enums;
+
+public enum UserRole {
+    CUSTOMER,
+    DELIVERY_PARTNER,
+    RESTAURANT_OWNER,
+    ADMIN
+}
